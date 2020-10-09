c9e408a9d5e62335df8495824f146f9498e8b9d2
Sarang <sarang98divakaran@gmail.com>
2020-10-09 15:43:58 +0530

    🌟 feat: parse torrent magnet uri, file or url

---
+++ src/modules/torrent/torrent.service.js
@@ -0,0 +1,81 @@
+const axios = require('axios');
+const WebTorrent = require('webtorrent');
+const parseTorrent = require('parse-torrent');
+const { isMagnetURI, isURL } = require('validator');
+
+const client = new WebTorrent();
+
+module.exports = {
+    /**
+     * Store queued torrent magnet uri, file or link
+     */
+    torrents: [],
+
+    /**
+     * Parse torrent from magnet, file or link to torrent file
+     * and return list of files.
+     *
+     * @param {Object} data
+     * @param {String|Object} data.torrent - Magnet URI, torrent file buffer or link to torrent file
+     */
+    async addTorrent(data = {}) {
+        if (!data.torrent) throw new Error('A torrent file, magnet uri or link to torrent file required.');
+
+        const metadata = await this.parse(data.torrent);
+        console.log(metadata);
+    },
+
+    /**
+     * Parse Torrent and return metadata.
+     *
+     * @param {String|Object} torrent - Magnet URI, torrent file buffer or link to torrent file
+     */
+    async parse(torrent) {
+        /**
+         * Torrent is parsed using parse-torrent library
+         * and a `so` property is added to the parsed Object.
+         *
+         * more details about why it is added is available in:
+         * https://github.com/webtorrent/webtorrent/issues/164#issuecomment-703489174
+         */
+        if (typeof torrent === 'object' || torrent instanceof Object) {
+            // Parse Torrent File buffer
+            try {
+                torrent = parseTorrent(torrent.buffer);
+                torrent.so = '-1';
+            } catch (err) {
+                throw new Error('Invalid Torrent File.');
+            }
+        } else if (isMagnetURI(torrent)) {
+            // Parse Magnet URI
+            try {
+                torrent = parseTorrent(torrent);
+                torrent.so = '-1';
+            } catch (err) {
+                throw new Error('Invalid Magnet URI.');
+            }
+        } else if (isURL(torrent)) {
+            // fetch torrent from url and parse
+            try {
+                const res = await axios.get(torrent, {
+                    responseType: 'arraybuffer'
+                });
+
+                torrent = parseTorrent(res.data);
+                torrent.so = '-1';
+            } catch (err) {
+                throw new Error('URL returned an invalid torrent.');
+            }
+        } else {
+            throw new Error('A valid torrent file, magnet uri or link to torrent file required.');
+        }
+
+        // Get complete metadata of the torrent using webtorrent client.
+        // when using magnet links parse-torrent does not provide list of files.
+        const metadata = await new Promise((resolve) => {
+            client.add(torrent, (t) => resolve(t));
+        });
+
+        return metadata;
+    }
+};
