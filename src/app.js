243eda417e01b74c5f2159764b53c8e2cf120923
Sarang <sarang98divakaran@gmail.com>
2020-10-17 23:15:50 +0530

    🌟 feat: added error handler in app and 404 handler in routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./modules');
+const { handler } = require('./utils/error');
 
 // create express app and server
 const app = express();
@@ -14,6 +15,9 @@ app.use(cors());
 
 // register routes
 routes(app);
+
+// Resiter Error handler
+app.use(handler);
 
 module.exports.app = app;
 module.exports.server = server;
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const { APIError } = require('../utils/error');
 
 // Import routes from each module
 const Torrent = require('./torrent/torrent.route')(router);
 
 module.exports = function (app) {
     app.use('/torrent', Torrent);
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        next(new APIError('Requested URL Not Found', 404));
+    });
 };
