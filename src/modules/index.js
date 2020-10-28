da788c487d513cc9ae7b2347bbadda80b2ea0d40
Sarang <sarang98divakaran@gmail.com>
2020-10-28 20:40:33 +0530

    🌟 feat: added users & auth routes

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,9 +3,11 @@ const { APIError } = require('../utils/error');
 
 // Import routes from each module
 const Torrent = require('./torrent/torrent.route')(router);
+const Users = require('./users/users.route')(router);
 
 module.exports = function (app) {
     app.use('/torrent', Torrent);
+    app.use(Users);
 
     // catch 404 and forward to error handler
     app.use(function (req, res, next) {
