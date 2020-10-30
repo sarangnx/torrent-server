9cdc51620d2876a91ade4e5d46d92c8dcf204c0d
Sarang <sarang98divakaran@gmail.com>
2020-10-30 20:30:46 +0530

    🌟 feat: createFolder method used to create folder in google drive recursively, when a path is given

--- src/utils/gapi.js
+++ src/utils/gapi.js
@@ -45,4 +45,92 @@ module.exports = class Gapi {
 
         return url;
     }
+
+    /**
+     * Cache already created folder's id here
+     */
+    folders = {};
+
+    /**
+     * Create folders recursively with BlackPearl as base folder
+     * ~/BlackPearl/path
+     *
+     * @param {String} path - Path string seperated by '/'
+     *
+     * @returns {String} Id of the innermost folder
+     */
+    async createFolder(path) {
+        // return cached value.
+        if (Object.keys(this.folders).includes(path) && this.folders[path].id) {
+            return this.folders[path].id;
+        }
+
+        const q = 'trashed=false and mimeType="application/vnd.google-apps.folder" and "me" in owners';
+
+        // get ~/BlackPearl folder in Drive
+        const res = await this.drive.files.list({
+            q: `${q} and name="BlackPearl" and "root" in parents`,
+            fields: 'files(id, name, parents)',
+            spaces: 'drive'
+        });
+
+        let root = res.data.files[0];
+
+        // If root folder ~/BlackPearl does not exist, create one.
+        if (!root || !res.data.files.length) {
+            const res = await this.drive.files.create({
+                resource: {
+                    name: 'BlackPearl',
+                    mimeType: 'application/vnd.google-apps.folder'
+                },
+                fields: 'id, name, parents'
+            });
+
+            root = res.data;
+        }
+
+        // set current directory as root for now
+        let cwd = Object.assign({}, root);
+        let parts = path.split('/');
+
+        for (let index in parts) {
+            let part = parts[index];
+            // current path
+            let p = parts.slice(0, parseInt(index) + 1).join('/');
+
+            // use cached value
+            if (Object.keys(this.folders).includes(p) && this.folders[p].id) {
+                cwd = Object.assign({}, this.folders[p]);
+                continue;
+            }
+
+            // search for folder in drive that has cwd as parent
+            const res = await this.drive.files.list({
+                q: `${q} and name="${part}" and "${cwd.id}" in parents`,
+                fields: 'files(id, name, parents)',
+                spaces: 'drive'
+            });
+
+            if (!res.data.files || !res.data.files.length) {
+                // create folder if it does not exist
+                const res = await this.drive.files.create({
+                    resource: {
+                        name: part,
+                        mimeType: 'application/vnd.google-apps.folder',
+                        parents: [cwd.id]
+                    },
+                    fields: 'id, name, parents'
+                });
+
+                cwd = Object.assign({}, res.data);
+            } else {
+                cwd = Object.assign({}, res.data.files[0]);
+            }
+
+            // push to cache
+            this.folders[p] = Object.assign({}, cwd);
+        }
+
+        return cwd.id;
+    }
 };
