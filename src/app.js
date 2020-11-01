994ace6b495a21eab5e6f236116b4baac24b131c
Sarang <sarang98divakaran@gmail.com>
2020-11-01 20:19:41 +0530

    🌟 feat: use socket.io in app

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./modules');
 const { handler } = require('./utils/error');
+const socket = require('./utils/socket');
 
 // create express app and server
 const app = express();
 const server = http.createServer(app);
+
+// bind http.Server to socket
+socket.attach(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
