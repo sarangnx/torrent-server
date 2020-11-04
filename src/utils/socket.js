fa2857d9359312fd5fac15d7d562bc9de520f5b6
Sarang <sarang98divakaran@gmail.com>
2020-11-04 23:08:31 +0530

    ✨ refactor: removed console.logs

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -25,11 +25,6 @@ class SocketIO {
      */
     init() {
         this.io.on('connection', (socket) => {
-            console.log('connected');
-            socket.on('disconnect', () => {
-                console.log('disconnected');
-            });
-
             socket.on('join:room', (roomId) => {
                 socket.join(roomId);
             });
